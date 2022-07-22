b0518143babe9d586577375d6f28f8dbf3034e9b
DigitalConcern <mihanik1608@gmail.com>
2022-07-22 21:36:46 +0300

    deleting info logs

--- source/application/photo/handlers.py
+++ source/application/photo/handlers.py
@@ -28,7 +28,6 @@ async def image_get_handler(request: web.Request):
         logger.debug('bad request - неверный параметр запроса', route=str(request.method) + " " + str(request.rel_url))
         return web.Response(status=400)
 
-    # logger.info('запрос на получение изображения', route=str(request.method) + " " + str(request.rel_url))
     connection = await database.connect()
     try:
         image_bin = await connection.fetchval("SELECT image_bin FROM photos_test WHERE id = $1", image_id)
@@ -36,8 +35,6 @@ async def image_get_handler(request: web.Request):
         logger.debug('изображение не найдено', route=str(request.method) + " " + str(request.rel_url))
         return web.Response(status=404)
     await connection.close()
-
-    # logger.info('изображение получено', route=str(request.method) + " " + str(request.rel_url))
 
     if image_bin is None:
         return web.Response(status=404)
@@ -48,7 +45,6 @@ async def image_get_handler(request: web.Request):
 async def image_post_handler(request: web.Request):
     image_bin = await request.read()
 
-    # logger.info('запрос на загрузку изображения', route=str(request.method) + " " + str(request.rel_url))
     with BytesIO() as bytes_stream:
         try:
             imageObj = Image.open(BytesIO(image_bin))
@@ -58,12 +54,7 @@ async def image_post_handler(request: web.Request):
             return web.Response(status=400)
 
         if imageObj.format != "JPEG":
-            # logger.info(f'получено изображение в формате {imageObj.format}',
-            #             route=str(request.method) + " " + str(request.rel_url))
             imageObj = imageObj.convert("RGB")
-        # else:
-            # logger.info(f'получено изображение в формате JPEG',
-            #             route=str(request.method) + " " + str(request.rel_url))
 
         try:
             x = request.rel_url.query["x"]
@@ -76,8 +67,6 @@ async def image_post_handler(request: web.Request):
             except ValueError:
                 logger.debug('bad request - неверный параметр запроса', route=str(request.method) + " " + str(request.rel_url))
                 return web.Response(status=400)
-            # logger.debug(f'необязательный параметр x',
-            #              route=str(request.method) + " " + str(request.rel_url))
         except KeyError:
             x = None
 
@@ -93,28 +82,20 @@ async def image_post_handler(request: web.Request):
                 logger.debug('bad request - неверный параметр запроса',
                              route=str(request.method) + " " + str(request.rel_url))
                 return web.Response(status=400)
-            # logger.debug(f'необязательный параметр y',
-            #              route=str(request.method) + " " + str(request.rel_url))
         except KeyError:
             y = None
 
         if x and y:
             imageObj.thumbnail((x, y))
-            # logger.info(f'размер изображения изменен по двум параметрам',
-            #             route=str(request.method) + " " + str(request.rel_url))
         # В случае, если задан только один параметр размера, второй высчитывается через начальное соотношение сторон
         elif x:
             x_percent = (x / float(imageObj.size[0]))
             y = int((float(imageObj.size[0]) * float(x_percent)))
             imageObj.thumbnail((x, y))
-            # logger.info(f'размер изображения изменен по параметру x',
-            #             route=str(request.method) + " " + str(request.rel_url))
         elif y:
             y_percent = (y / float(imageObj.size[1]))
             x = int((float(imageObj.size[1]) * float(y_percent)))
             imageObj.thumbnail((x, y))
-            # logger.info(f'размер изображения изменен по параметру y',
-            #             route=str(request.method) + " " + str(request.rel_url))
 
         try:
             quality = request.rel_url.query["quality"]
@@ -125,8 +106,6 @@ async def image_post_handler(request: web.Request):
                              route=str(request.method) + " " + str(request.rel_url))
                 return web.Response(status=400)
             imageObj.save(bytes_stream, format='JPEG', quality=quality)
-            # logger.debug(f'качество изображения изменено',
-            #              route=str(request.method) + " " + str(request.rel_url))
         except KeyError:
             imageObj.save(bytes_stream, format='JPEG')
 
@@ -138,8 +117,6 @@ async def image_post_handler(request: web.Request):
     await connection.execute("INSERT INTO photos_test (id, image_bin) VALUES ($1, $2)", unique_id, bytes_arr)
     await connection.close()
 
-    # logger.info(f'изображение успешно сохранено',
-    #             route=str(request.method) + " " + str(request.rel_url))
     return web.Response(text=str(unique_id), status=200)
 
 
@@ -165,8 +142,6 @@ async def registration_handler(request: web.Request):
         return web.Response(status=400)
 
     unique_user_id = uuid.uuid5(uuid.NAMESPACE_DNS, email + password)
-
-    # logger.info('запрос на регистрацию', route=str(request.method) + " " + str(request.rel_url))
 
     access_token = uuid.uuid4()
 
@@ -211,8 +186,6 @@ async def login_handler(request: web.Request):
         logger.debug('bad request  - некорректный json', route=str(request.method) + " " + str(request.rel_url))
         return web.Response(status=400)
 
-    # logger.info('запрос на вход', route=str(request.method) + " " + str(request.rel_url))
-
     access_token = uuid.uuid4()
 
     connection = await database.connect()
@@ -235,13 +208,9 @@ async def login_handler(request: web.Request):
 
     await connection.close()
 
-    # logger.info('пользователь авторизован', route=str(request.method) + " " + str(request.rel_url))
-
     return web.Response(text=str(access_token), status=200)
 
 
 async def logs_handler(request: web.Request):
     logs = open("myapp.log")
-    # logger.info(f'выгружены логи',
-    #             route=str(request.method) + " " + str(request.rel_url))
     return web.Response(text="".join(logs.readlines()), status=200)
