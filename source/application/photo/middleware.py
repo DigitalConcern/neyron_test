13b82d2208096257862f57fc0041f9ef61b4c1a4
DigitalConcern <mihanik1608@gmail.com>
2022-07-22 21:54:41 +0300

    fixing bugs

--- source/application/photo/middleware.py
+++ source/application/photo/middleware.py
@@ -18,7 +18,6 @@ logger = CustomAdapter(logger, {"route": None})
 @web.middleware
 async def auth_required_middleware(request, handler):
     if str(request.rel_url) != '/login' and str(request.rel_url) != '/registration':
-        # делаем срез для получения токена
         try:
             token = UUID(request.headers.get('Authorization')[7:])
         except:
@@ -34,17 +33,16 @@ async def auth_required_middleware(request, handler):
 
         if time_create is None:
             logger.debug('запрос без авторизации', route=str(request.method) + " " + str(request.rel_url))
+            await connection.close()
             return web.Response(status=401)
 
         if (datetime.datetime.now() - time_create).total_seconds() < 10*60:
             logger.debug('авторизованный запрос', route=str(request.method) + " " + str(request.rel_url))
             await connection.close()
             return await handler(request)
-
-        await connection.close()
-
-        # logger.debug('запрос без авторизации', route=str(request.method) + " " + str(request.rel_url))
-
-        return web.Response(status=401)
+        else:
+            logger.debug('запрос без авторизации', route=str(request.method) + " " + str(request.rel_url))
+            await connection.close()
+            return web.Response(status=401)
     else:
         return await handler(request)
